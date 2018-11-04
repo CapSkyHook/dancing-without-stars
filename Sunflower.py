93a9be896cda2921a5a925f6a39ff590cc6fd351
Yahui Cui <cuiyahui1105@gmail.com>
2018-11-04 18:28:51 -0500

    correct

--- Sunflower.py
+++ Sunflower.py
@@ -240,10 +240,10 @@ class Player:
 					moves_this_turn[it] = bfs.pop()
 					for dancerId in range(it+1, int(len(self.dancers))):
 						curr_pos = curr_poses[dancerId]
-				 try:
-					 end_pos = end_coordinates[dancerId]
-				 except Exception as e:
-					 import pdb; pdb.set_trace()
+				  try:
+					  end_pos = end_coordinates[dancerId]
+				  except Exception as e:
+					  import pdb; pdb.set_trace()
 
 				  valid_moves = self.find_viable_moves(curr_pos, end_pos, board)
 
